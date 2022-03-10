9507239fedc8ba7c1e535002c4764c02fbbe260a
Mushfiq Mashuk <alberteinstein3626@gmail.com>
2022-03-11 02:22:51 +0600

    Dynamic typing

--- output/script.js
+++ output/script.js
@@ -14,6 +14,7 @@ const colors = ["red", "blue", "green", 34];
 const obj = {
     name: "mash",
     age: 34,
+    isStudent: true,
 };
 colors.push(false);
 console.log(colors);
@@ -22,3 +23,44 @@ function printName(person) {
     console.log(person.name);
 }
 printName(obj);
+//Explicit typing and Enum typing
+// Enum
+var UserResponse;
+(function (UserResponse) {
+    UserResponse[UserResponse["Yes"] = 45] = "Yes";
+    UserResponse[UserResponse["No"] = 46] = "No";
+    UserResponse[UserResponse["Maybe"] = 47] = "Maybe";
+})(UserResponse || (UserResponse = {}));
+console.log(UserResponse);
+console.log(UserResponse["Yes"]);
+// Union
+// Array typing
+let x;
+x = [23, 344, "dfsdf"];
+// Object typing
+let arr;
+arr = {
+    name: "MAshuk",
+};
+arr = [2, 4, 54];
+// Object typing
+let arr2;
+arr2 = {
+    name: "Mashuk",
+    age: 24,
+    country: "BD", // error
+};
+// Dynamic type or any type
+let any;
+any = 45;
+any = "mash";
+// any object type
+let anyObject;
+anyObject = {
+    name: "Mashuk",
+    age: "21",
+};
+// any array
+let anyArray = [];
+anyArray.push(5);
+anyArray.push(false);
--- src/script.ts
+++ src/script.ts
@@ -19,11 +19,14 @@ const colors = ["red", "blue", "green", 34];
 const obj = {
   name: "mash",
   age: 34,
+  isStudent: true,
 };
 
 colors.push(false);
 console.log(colors);
 console.log(obj.country);
+
+// TypeScript Interface
 
 interface Person {
   name: string;
@@ -36,3 +39,71 @@ function printName(person: Person) {
 }
 
 printName(obj);
+
+//Explicit typing and Enum typing
+
+// Enum
+enum UserResponse {
+  Yes = 45,
+  No,
+  Maybe,
+}
+
+console.log(UserResponse);
+console.log(UserResponse["Yes"]);
+
+// Union
+
+// Array typing
+let x: (string | number)[];
+
+x = [23, 344, "dfsdf"];
+
+// Object typing
+
+let arr: object;
+
+arr = {
+  name: "MAshuk",
+};
+
+arr = [2, 4, 54];
+
+// Object typing
+
+let arr2: {
+  name: string;
+  age: number;
+};
+
+arr2 = {
+  name: "Mashuk",
+  age: 24,
+  country: "BD", // error
+};
+
+// Dynamic type or any type
+
+let any: any;
+
+any = 45;
+any = "mash";
+
+// any object type
+
+let anyObject: {
+  name: any;
+  age: any;
+};
+
+anyObject = {
+  name: "Mashuk",
+  age: "21",
+};
+
+// any array
+
+let anyArray: any[] = [];
+
+anyArray.push(5);
+anyArray.push(false);
