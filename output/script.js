a6115c560a843213693aa37218cdbe5984909980
Mushfiq Mashuk <alberteinstein3626@gmail.com>
2022-03-10 19:27:17 +0600

    Array & Strings

--- output/script.js
+++ output/script.js
@@ -1,12 +1,24 @@
 "use strict";
 class Nice {
-  constructor() {
-    console.log("Class");
-  }
+    constructor() {
+        console.log("Class");
+    }
 }
 new Nice();
-let a = 45;
+let a = "";
 function assignA(arg = {}) {
-  console.log(arg);
+    console.log(arg);
 }
 assignA(a);
+const colors = ["red", "blue", "green", 34];
+const obj = {
+    name: "mash",
+    age: 34,
+};
+colors.push(false);
+console.log(colors);
+console.log(obj.country);
+function printName(person) {
+    console.log(person.name);
+}
+printName(obj);
--- src/script.ts
+++ src/script.ts
@@ -6,10 +6,33 @@ class Nice {
 
 new Nice();
 
-let a: string = 45;
+let a: string = "";
 
 function assignA(arg: object = {}) {
   console.log(arg);
 }
 
 assignA(a);
+
+const colors = ["red", "blue", "green", 34];
+
+const obj = {
+  name: "mash",
+  age: 34,
+};
+
+colors.push(false);
+console.log(colors);
+console.log(obj.country);
+
+interface Person {
+  name: string;
+  age: number;
+  isStudent: boolean;
+}
+
+function printName(person: Person) {
+  console.log(person.name);
+}
+
+printName(obj);
